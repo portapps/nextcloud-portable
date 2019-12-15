8a1d11a6bebb24f7f0d1a29fe3c43b0748a2203d
CrazyMax <crazy-max@users.noreply.github.com>
2019-12-15 11:33:09 +0100

    Add cleanup config

--- main.go
+++ main.go
@@ -4,20 +4,31 @@ package main
 
 import (
 	"os"
+	"path"
 
 	. "github.com/portapps/portapps"
 	"github.com/portapps/portapps/pkg/utl"
 )
 
+type config struct {
+	Cleanup bool `yaml:"cleanup" mapstructure:"cleanup"`
+}
+
 var (
 	app *App
+	cfg *config
 )
 
 func init() {
 	var err error
 
+	// Default config
+	cfg = &config{
+		Cleanup: false,
+	}
+
 	// Init app
-	if app, err = New("nextcloud-portable", "Nextcloud"); err != nil {
+	if app, err = NewWithCfg("nextcloud-portable", "Nextcloud", cfg); err != nil {
 		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
 	}
 }
@@ -31,6 +42,15 @@ func main() {
 	app.Args = []string{
 		"--confdir",
 		confPath,
+	}
+
+	// Cleanup on exit
+	if cfg.Cleanup {
+		defer func() {
+			utl.Cleanup([]string{
+				path.Join(os.Getenv("LOCALAPPDATA"), "Nextcloud"),
+			})
+		}()
 	}
 
 	app.Launch(os.Args[1:])
